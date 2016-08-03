37cdb5b0babdfd94e08a18190b21a9f4860d8398
aoancea <alx.oancea@gmail.com>
2016-08-03 21:06:29 +0300

    Initial structure

---
+++ src/Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tutorial.Material.Design.Web.MvcApplication" Language="C#" %>
---
+++ src/Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tutorial.Material.Design.Web.Startup))]
+namespace Tutorial.Material.Design.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
