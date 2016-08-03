ffd5c8df28a8170f85811b47be7f26ab97e9cee1
aoancea <alx.oancea@gmail.com>
2016-08-03 21:18:39 +0300

    Added tables page

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -6,25 +6,32 @@ using System.Web.Mvc;
 
 namespace Tutorial.Material.Design.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-    }
+			return View();
+		}
+
+		public ActionResult Tables()
+		{
+			ViewBag.Message = "Tables";
+
+			return View();
+		}
+	}
 }
